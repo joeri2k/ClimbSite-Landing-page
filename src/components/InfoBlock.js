353d038fe6b7b456e8b763b86188d607824ee21f
joe rizk <joerizk2@hotmail.com>
2022-03-01 13:58:05 +0200

    rendering infp

--- src/components/InfoBlock.js
+++ src/components/InfoBlock.js
@@ -1,17 +1,7 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
 
 export default function InfoClock({ text, title }) {
   return (
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,33 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import NavbarLoggedIn from "../components/NavbarLoggedIn";
-import bg from "../images/bg.jpg";
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
 const Dashboard = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const url = "http://127.0.0.1:8000/api/auth/user-profile";
+  const token = localStorage.getItem("token");
+  useEffect(() => {
+    async function handleSubmit() {
+      try {
+        const response = await axios.get(url, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        const data_received = await response.data;
+        console.log(data_received);
+        setName(data_received.name);
+        setEmail(data_received.email);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    handleSubmit();
+  }, []);
+
   return (
     <div>
       <NavbarLoggedIn />
@@ -16,11 +42,34 @@ const Dashboard = () => {
           color: "white",
         }}
       >
-        <span>{localStorage.getItem("name")}</span>
-        <br />
-        <span>{localStorage.getItem("email")}</span>
-        <br />
-        <span>{localStorage.getItem("token")}</span>
+        <h1 style={{ color: "white ", textAlign: "center" }}>{title}</h1>
+
+        <Container
+          component="main"
+          maxWidth="sm"
+          style={{
+            backgroundColor: "#2F3F4A",
+            borderRadius: "15px",
+            color: "white",
+          }}
+        >
+          <CssBaseline />
+          <Box
+            style={{
+              padding: "20px",
+              fontSize: "18px",
+              lineHeight: "30px",
+              textAlign: "justify",
+            }}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            {text}
+          </Box>
+        </Container>
       </div>
     </div>
   );
